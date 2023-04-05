1241f5c63b4f3c6145eab4a29102456d5afdbf4a
PeppeCaccia <giuseppecacciapuoti@dhcp-v34-192-168-68-137.pdl.cineca.it>
2023-04-05 09:28:05 +0200

    Initial Commit

---
+++ target/.generated/com/google/gwt/lang/it_00046unibo_00046sweng_00046simplegwt_00046SimpleGwt__EntryMethodHolder.java
@@ -0,0 +1,6 @@
+package com.google.gwt.lang;
+public class it_00046unibo_00046sweng_00046simplegwt_00046SimpleGwt__EntryMethodHolder {
+  public static final void init() {
+    // to be synthesized later
+  }
+}
---
+++ target/.generated/it/unibo/sweng/simplegwt/client/Messages_.java
@@ -0,0 +1,12 @@
+package it.unibo.sweng.simplegwt.client;
+
+public class Messages_ implements it.unibo.sweng.simplegwt.client.Messages {
+  
+  public java.lang.String nameField() {
+    return "Enter your name";
+  }
+  
+  public java.lang.String sendButton() {
+    return "Send";
+  }
+}
